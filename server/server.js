604d4ed5659db8a4b009a537e4cea16abe896c69
Woojae Kim <kwj280@gmail.com>
2021-12-10 15:16:21 -0700

    increase payload size

--- server/server.js
+++ server/server.js
@@ -20,9 +20,8 @@ const port = process.env.PORT || 5000
 // app.use(cors())
 
 app.use(session({ secret: "cats" }));
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json())
+app.use(express.json({limit: '10mb'}))
+app.use(bodyParser.urlencoded({ extended: false, limit:'10mb' }));
 app.use(passport.initialize());
 app.use(passport.session());
 

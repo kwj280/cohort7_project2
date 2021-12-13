c5206ef312d9087eef0a244b3053c3153a73d0d3
Clayton Balzer <clayton.balzer.dev@gmail.com>
2021-12-13 11:12:20 -0700

    added dynamic function to JobCard from JSON data

--- server/server.js
+++ server/server.js
@@ -17,21 +17,16 @@ dotenv.config()
 const port = process.env.PORT || 5000
 
 //server config
-// app.use(cors())
-app.use('/job', jobRouter)
-app.use('/profile', profileRouter)
-app.use('/', express.static('../client/build'))
-app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname,"../client/build","index.html"))
-})
 app.use(session({ secret: "cats" }));
 app.use(express.json({limit: '10mb'}))
 app.use(bodyParser.urlencoded({ extended: false, limit:'10mb' }));
 app.use(passport.initialize());
 app.use(passport.session());
-
+// app.use(cors())
+app.use('/job', jobRouter)
+app.use('/profile', profileRouter)
 app.get('/api/jobs', (req, res) => {
-  console.log('Hello')
+  console.log(jobData)
   res.status(200).json(jobData)
   
   
@@ -40,6 +35,11 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ success:true })
 });
 app.use('/user', userRouter)
+app.use('/', express.static('../client/build'))
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname,"../client/build","index.html"))
+})
+
 
 
 

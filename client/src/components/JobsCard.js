4eb9b5a125de10ad6a53d0c76a71e12e74b1a24b
Clayton Balzer <clayton.balzer.dev@gmail.com>
2021-12-12 21:03:13 -0700

    reconfigured JobCard and JobPages

--- client/src/components/JobsCard.js
+++ client/src/components/JobsCard.js
@@ -1,64 +1,78 @@
-import * as React from 'react'
-import { useState } from 'react'
-import { styled } from '@mui/material/styles'
-import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
-import CardMedia from '@mui/material/CardMedia'
-import CardContent from '@mui/material/CardContent'
-import CardActions from '@mui/material/CardActions'
-import Collapse from '@mui/material/Collapse'
-import Avatar from '@mui/material/Avatar'
-import IconButton from '@mui/material/IconButton'
-import Typography from '@mui/material/Typography'
-import { red } from '@mui/material/colors'
-import FavoriteIcon from '@mui/icons-material/Favorite'
-import ShareIcon from '@mui/icons-material/Share'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
-
+import React, { useState, useEffect } from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import axios from 'axios'
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props
-  return <IconButton {...other} />
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
 })(({ theme, expand }) => ({
   transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
   marginLeft: 'auto',
   transition: theme.transitions.create('transform', {
     duration: theme.transitions.duration.shortest,
   }),
-}))
+}));
 
-function JobsCard() {
-  const [expanded, setExpanded] = useState(false)
-  const [title,getTitle] = useState([])
+export default function JobCard() {
+  const [expanded, setExpanded] = useState(false);
+const [jobs, setJobs] = useState('')
   const handleExpandClick = () => {
-    setExpanded(!expanded)
-  }
+    setExpanded(!expanded);
+  };
+  useEffect(() => {
+    async function fetchData() {
+        console.log('Fetching Job data!')
+        let fetchResult = await fetch('/api/jobs')
+        let JobList = await fetchResult.json()
+        setJobs(JobList)
+    }
+    fetchData()
+}, [])
 
   return (
-    <Card sx={{ }}>
+    <Card >
       <CardHeader
-        avatar={<Avatar sx={{ bgColor: red[500] }} aria-label="jobs"></Avatar>}
+        avatar={
+          <Avatar sx={{ bgcolor: red[500] }} aria-label="jobs">
+            R
+          </Avatar>
+        }
         action={
           <IconButton aria-label="settings">
             <MoreVertIcon />
           </IconButton>
         }
-        title="title"
-        subheader={new Date().toLocaleString()}
+        {...jobs &&
+          jobs.map((job) => {
+            return (
+              <ul key={job.id}>
+                <li>{job.title}</li>
+              </ul>
+            )
+          })}
+        title= "Software Developer"
+        subheader="September 14, 2016"
       />
-      {/* <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      /> */}
+     
       <CardContent>
-        <Typography variant="body2" color="text.secondary"
-        title="Job Description"
-        >Job Description
-
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the mussels,
+          if you like.
         </Typography>
-        
       </CardContent>
       <CardActions disableSpacing>
         <IconButton aria-label="add to favorites">
@@ -80,35 +94,31 @@ function JobsCard() {
         <CardContent>
           <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            {/* Heat 1/2 cup of the broth in a pot until simmering, add saffron and
-            set aside for 10 minutes. */}
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+            aside for 10 minutes.
           </Typography>
           <Typography paragraph>
-            {/* Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-            over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-            stirring occasionally until lightly browned, 6 to 8 minutes.
-            Transfer shrimp to a large plate and set aside, leaving chicken and
-            chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes,
-            onion, salt and pepper, and cook, stirring often until thickened and
-            fragrant, about 10 minutes. Add saffron broth and remaining 4 1/2
-            cups chicken broth; bring to a boil. */}
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
+            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
+            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
+            large plate and set aside, leaving chicken and chorizo in the pan. Add
+            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
+            stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
           </Typography>
           <Typography paragraph>
-            {/* Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is
-            absorbed, 15 to 18 minutes. Reduce heat to medium-low, add reserved
-            shrimp and mussels, tucking them down into the rice, and cook again
-            without stirring, until mussels have opened and rice is just tender,
-            5 to 7 minutes more. (Discard any mussels that don’t open.) */}
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
           </Typography>
           <Typography>
-            {/* Set aside off of the heat to let rest for 10 minutes, and then
-            serve. */}
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
         </CardContent>
       </Collapse>
     </Card>
-  )
+  );
 }
-
-export default JobsCard
--- client/src/pages/JobsPage.js
+++ client/src/pages/JobsPage.js
@@ -1,11 +1,9 @@
-import React, { Component, useState, useEffect } from 'react'
-
-import JobCard from '../components/JobsCard'
+import * as React from 'react'
 import { styled } from '@mui/material/styles'
 import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
-
+import JobCard from '../components/JobsCard'
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
   padding: theme.spacing(1),
@@ -13,39 +11,23 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }))
 
-export default function BasicGrid(props) {
-  const [jobs, setJobs] = useState('')
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [skills, setSkills] = useState([])
-  const [company, setCompany] = useState('')
-  const [url, setUrl] = useState('')
-  useEffect(() => {
-    fetch('/api/jobs')
-      .then((response) => response.json())
-      .then((json) => setJobs(json))
-  }, [])
+const JobGrid = () => {
   return (
-    <>
-      <h1 className="App">Job Postings</h1>
-      <Box sx={{ flexGrow: 2 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Item>
-              
-              {jobs &&
-                jobs.map((job,i) => {
-                  return <p key={i}>{job.title}</p>
-                })}
-            </Item>
-          </Grid>
-          <Grid item xs={8}>
-            <Item>
-              <JobCard title="Job Title" />
-            </Item>
-          </Grid>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Item>
+            <JobCard />
+          </Item>
         </Grid>
-      </Box>
-    </>
+        <Grid item xs={6}>
+          <Item>
+            <JobCard />
+          </Item>
+        </Grid>
+      </Grid>
+    </Box>
   )
 }
+
+export default JobGrid

352765e84bf7109aafbb8fadc742dd5d5e8f9b3d
Brian Sauco <brian.sauco.95@gmail.com>
2021-12-16 23:41:55 -0700

    Account settings page works

--- client/src/pages/SettingPage.js
+++ client/src/pages/SettingPage.js
@@ -1,5 +1,11 @@
 import Container from "@mui/material/Container";
+import { styled } from "@mui/system";
 import Paper from "@mui/material/Paper";
+import EditIcon from "@mui/icons-material/Edit";
+import { Grid, IconButton, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import Button from "@mui/material/Button";
+import MuiTextField from "@mui/material/TextField";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -12,11 +18,22 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SettingPage({ user }) {
   const classes = useStyles();
+  const [profile, setProfile] = useState();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
+  const [editInfo, setEditInfo] = useState(false);
   const theme = useTheme();
+
+  const TextField = styled(MuiTextField)({
+    width: "100%",
+  });
+  const ListItem = styled("li")(({ theme }) => ({
+    margin: theme.spacing(0.5),
+    float: "left",
+  }));
+
   useEffect(() => {
     setFirstName(user?.firstName);
     setLastName(user?.lastName);
@@ -35,6 +52,7 @@ export default function SettingPage({ user }) {
       id: user._id,
     });
   };
+
   console.log(user);
   return (
     <>
@@ -48,15 +66,125 @@ export default function SettingPage({ user }) {
             alignItems: "center",
           }}
         >
-          <h1>Setting Components here</h1>
-          <ul>
+          <Grid
+            container
+            justifyContent="center"
+            alignItems="center"
+            justifyContent="center"
+            rowSpacing={2}
+            columnSpacing={2}
+          >
+            <Grid item xs={12} container justifyContent="center">
+              <Typography variant="h3" sx={{ paddingBottom: "0px" }}>
+                Account Settings
+              </Typography>{" "}
+              {!editInfo && (
+                <Button
+                  sx={{ paddingX: "16px" }}
+                  onClick={() => {
+                    setEditInfo(true);
+                  }}
+                >
+                  <EditIcon />
+                </Button>
+              )}
+            </Grid>
+            {!editInfo ? (
+              <>
+                <Grid item xs={12} container justifyContent="center">
+                  <Typography variant="p" sx={{ paddingX: "12px" }}>
+                    <strong>First Name:</strong> {firstName}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <Typography variant="p" sx={{ paddingY: "0px" }}>
+                    <strong>Last Name:</strong> {lastName}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <Typography variant="p" sx={{ paddingX: "12px" }}>
+                    <strong>Password:</strong> {password.length} characters long
+                  </Typography>
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <Typography variant="p" sx={{ paddingX: "12px" }}>
+                    <strong>Email:</strong> {email}
+                  </Typography>
+                </Grid>
+              </>
+            ) : (
+              <>
+                <Grid item xs={12} container justifyContent="center">
+                  <TextField
+                    id="outlined-basic"
+                    label="First Name"
+                    variant="outlined"
+                    value={firstName}
+                    size="small"
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <TextField
+                    id="outlined-basic"
+                    label="Last Name"
+                    variant="outlined"
+                    value={lastName}
+                    size="small"
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <TextField
+                    id="outlined-password-input"
+                    label="Password"
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                  />
+                </Grid>
+                <Grid item xs={12} container justifyContent="center">
+                  <TextField
+                    id="outlined-basic"
+                    label="Email"
+                    variant="outlined"
+                    value={email}
+                    size="small"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                </Grid>
+                <Button
+                  onClick={() => {
+                    setEditInfo(false);
+                    updateUser();
+                  }}
+                >
+                  Save
+                </Button>
+              </>
+            )}
+          </Grid>
+
+          {/* <ul>
             <li>
-              <input
-                value={firstName}
-                onChange={(e) => {
-                  setFirstName(e.target.value);
-                }}
-              />
+              <p>{firstName}</p>
+              {editInfo === true && (
+                <input
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                  }}
+                />
+              )}
+              <button>Edit</button>
             </li>
             <li>
               <input
@@ -90,7 +218,7 @@ export default function SettingPage({ user }) {
             }}
           >
             Save
-          </button>
+          </button> */}
         </Paper>
       </Container>
     </>
